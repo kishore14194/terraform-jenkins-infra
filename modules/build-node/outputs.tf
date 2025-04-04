ebdb11b91782b19ad64521e7deaa25f9c964e61d
kishore14194 <thereal.hunter33@gmail.com>
2025-04-04 10:17:46 +0530

    Modifying output files

--- modules/build-node/outputs.tf
+++ modules/build-node/outputs.tf
@@ -1,4 +1,4 @@
-output "build-node-worker-ip" {
+output "build_node_worker_ip" {
   description = "Public IP address of Build Node"
   value       = aws_instance.build_node.public_ip
 }
--- output.tf
+++ output.tf
@@ -1,7 +1,12 @@
-# output "jenkins_public_ip" {
-#   value = aws_instance.jenkins.public_ip
-# }
-#
-# output "build_node_public_ip" {
-#   value = aws_instance.build_node.public_ip
-# }
+# Fetch module outputs
+output "jenkins_ip" {
+  value = module.jenkins.jenkins_public_ip
+}
+
+output "build_node_ip" {
+  value = module.build_node.build_node_worker_ip
+}
+
+output "ansible_node_ip" {
+  value = module.ansible.ansible_node_worker_ip
+}
